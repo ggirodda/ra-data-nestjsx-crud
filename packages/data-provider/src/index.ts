e0f3072e23d8f5f32e85e48f0dd4462c80fd4b3b
Marceau Meignent <marceau.meignent@neoxia.com>
2020-10-19 17:31:00 +0200

    Fixed formData conversion for nested Objects & Files

--- packages/data-provider/src/index.ts
+++ packages/data-provider/src/index.ts
@@ -122,16 +122,35 @@ const getParamsWithFileUploadFiles = async (data, fileUploadKey) => {
   if (fileUpload === undefined) {
     return data;
   }
-  await Promise.all(Object.keys(fileUpload).map(async (key) => {
-    dataPayload[key] = await transformFileIntoFormDataFile(fileUpload[key]);
-  }));
-  const formData = new FormData();
-  Object.keys(dataPayload).forEach((key) => {
-    key = Array.isArray(dataPayload[key]) ? `${key}[]` : key
-    formData.append(key, dataPayload[key])
-  })
+  await Promise.all(
+    Object.keys(fileUpload).map(async (key) => {
+      dataPayload[key] = await transformFileIntoFormDataFile(fileUpload[key]);
+    })
+  );
+  const formData = createFormData(dataPayload);
   return formData;
 };
+
+function createFormData(object: Object, form?: FormData, namespace?: string): FormData {
+  const formData = form || new FormData();
+  for (let property in object) {
+    if (
+      !object.hasOwnProperty(property) ||
+      (object[property] == null && object[property] === undefined)
+    ) {
+      continue;
+    }
+    const formKey = namespace ? `${namespace}[${property}]` : property;
+    if (object[property] instanceof Date) {
+      formData.append(formKey, object[property].toISOString());
+    } else if (typeof object[property] === 'object' && !(object[property] instanceof File)) {
+      createFormData(object[property], formData, formKey);
+    } else {
+      formData.append(formKey, object[property]);
+    }
+  }
+  return formData;
+}
 
 export default (
   apiUrl: string,
